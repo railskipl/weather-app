9d7240242733cf2069dd85c4d438022897d5dbe4
railskipl <rails@knalinfotech.net>
2018-03-01 15:50:30 +0530

    change in logic

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,23 +3,43 @@ class WeatherController < ApplicationController
 
 	def show
 
-		if get_weather_1 == "Clear"
-			@recommended_city = get_city_1["city"]
-		elsif get_weather_2 == "Clear"
-			@recommended_city = get_city_2["city"]
-		elsif get_weather_3 == "Clear"
-			@recommended_city = get_city_3["city"]
-		elsif get_weather_4 == "Clear"
-			@recommended_city = get_city_4["city"]
-		else get_weather_5 == "Clear"
-			@recommended_city = get_city_5["city"]
-		end				
+		all_temperature = [get_city_1["temp"],get_city_2["temp"],get_city_3["temp"],get_city_4["temp"],get_city_5["temp"]].sort!
+		max_temp = all_temperature.last
+		min_temp = all_temperature.first
+		@avg_temp = (max_temp + min_temp)/2
+		r1 = (@avg_temp-5)
+		r2 = (@avg_temp+5)
+
+		data1 = []
+
+		data1 << {"city" => get_city_1["city"], "temp" => get_city_1["temp"]} if (get_weather_1 == "Clear") && get_city_1["temp"].between?(r1,r2)
+		data1 << {"city" => get_city_2["city"], "temp" => get_city_2["temp"]} if (get_weather_2 == "Clear") && get_city_2["temp"].between?(r1,r2)
+		data1 << {"city" => get_city_3["city"], "temp" => get_city_3["temp"]} if (get_weather_3 == "Clear") && get_city_3["temp"].between?(r1,r2)
+		data1 << {"city" => get_city_4["city"], "temp" => get_city_4["temp"]} if (get_weather_4 == "Clear") && get_city_4["temp"].between?(r1,r2)
+		data1 << {"city" => get_city_5["city"], "temp" => get_city_5["temp"]} if (get_weather_5 == "Clear") && get_city_5["temp"].between?(r1,r2)		
+
+		data2 = []
+
+		data2 << {"city" => get_city_1["city"], "temp" => get_city_1["temp"]} if (get_weather_1 == "Clouds") && get_city_1["temp"].between?(r1,r2)
+		data2 << {"city" => get_city_2["city"], "temp" => get_city_2["temp"]} if (get_weather_2 == "Clouds") && get_city_2["temp"].between?(r1,r2)
+		data2 << {"city" => get_city_3["city"], "temp" => get_city_3["temp"]} if (get_weather_3 == "Clouds") && get_city_3["temp"].between?(r1,r2)
+		data2 << {"city" => get_city_4["city"], "temp" => get_city_4["temp"]} if (get_weather_4 == "Clouds") && get_city_4["temp"].between?(r1,r2)
+		data2 << {"city" => get_city_5["city"], "temp" => get_city_5["temp"]} if (get_weather_5 == "Clouds") && get_city_5["temp"].between?(r1,r2)		
+
+		@recommended_city = ''
+
+		if data1.empty?
+			arr1 = []
+			final_weather = data2.max_by { |x| x["temp"].to_f}
+			@recommended_city = final_weather["city"]
+		else
+			arr2 = []
+			final_weather2 = data1.max_by { |x| x["temp"].to_f}
+			@recommended_city = final_weather2["city"]	
+		end	
 		@weather_all_cities = [get_city_1, get_city_2, get_city_3, get_city_4, get_city_5]
-  	if @recommended_city.empty?
-      render :show, notice: "City Not Found."
-    else
-      render :show
-    end		
+
+		render :show	
 	end
 
 	def get_weather_1 
@@ -57,22 +77,22 @@ class WeatherController < ApplicationController
 	end	
 
 	def get_city_3
-		city_3 = "Sydney"
-		county_code_3 = "AU"
+		city_3 = "stuttgart"
+		county_code_3 = "DE"
 		weather_3 = open_weather_api.current city: city_3, country_code: county_code_3
 		parse_weather(weather_3)
 	end	
 
 	def get_city_4
-		city_4 = "London"
-		county_code_4 = "UK"
+		city_4 = "Hamburg"
+		county_code_4 = "DE"
 		weather_4 = open_weather_api.current city: city_4, country_code: county_code_4
 		parse_weather(weather_4)
 	end			
 
 	def get_city_5
-		city_5 = "New York"
-		county_code_5 = "US"
+		city_5 = "Dresden"
+		county_code_5 = "DE"
 		weather_5 = open_weather_api.current city: city_5, country_code: county_code_5
 		parse_weather(weather_5)
 	end				
